62103e999f4723e844073f018a4e4ea62abbffea
Gbadass <theboyfakaa@gmail.com>
2023-03-03 04:19:52 -0800

    i created a calculator

---
+++ index.js
@@ -0,0 +1,125 @@
+//document.getElementById('count-el').innerText = 5
+
+// let bonusCount = 5
+// bonusCount = bonusCount + 50
+// bonusCount = bonusCount - 75
+// bonusCount = bonusCount + 55
+
+// console.log(bonusCount)
+
+// let saveEl = document.getElementById('save-el')
+// let countEl = document.getElementById('count-el')
+// let count = 0
+
+// console.log(saveEl)
+
+// function increment() {
+//   count += 1
+//   countEl.textContent = count
+
+// }
+
+
+
+// function save() {
+//   let countStr = count + " - "
+//    saveEl.textContent += countStr
+//    countEl.textContent = 0
+//    count = 0
+// }
+
+// function save() {
+//   console.log(count)
+// }
+
+// save()
+
+// let name = 'Gbadass'
+// let greeting = 'hello'
+
+// myGreeting = greeting + "," + name + "!"
+
+// console.log(myGreeting)
+
+
+// let Name = 'Gbadass'
+// let greeting = 'hello'
+
+// function greetlinda() {
+//   console.log(greeting + ',' + ' ' + Name + '!' )
+// }
+
+// greetlinda()
+
+// let myPoint = 3
+
+// function add3points(){
+//   myPoint += 3
+
+// }
+
+
+// function remove1point() {
+//   myPoint -= 1
+
+// }
+
+// add3points()
+// add3points()
+// add3points()
+// remove1point()
+// remove1point()
+
+// console.log(myPoint)
+
+let num1 = 8
+let num2 = 4
+
+ let enterValue = document.getElementById('enter-value').textContent = num1
+ let enteredV = document.getElementById('enteredV').textContent = num2
+
+
+
+
+//  console.log(valueStr)
+//  console.log(valueStr2)
+
+
+
+//  function valueStr() {
+//   enterValue.innerText = num1
+
+//  }
+ 
+//  valueStr()
+
+//  function valueStr2() {
+//   enteredV.innerText = num2
+//  }
+
+//  valueStr2()
+
+ let sumEl = document.getElementById('sum-el')
+
+ function add() {
+  sumEl.innerText = num1 + num2
+ }
+
+
+ function subtract() {
+  sumEl.innerText = num1 - num2
+ }
+
+
+ function multiply() {
+  sumEl.innerText = num1 * num2
+ }
+
+
+ function divide() {
+  sumEl.innerText = num1 / num2
+ }
+
+
+
+
